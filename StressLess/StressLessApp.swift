78162b75d3a44f8ea5f747408849ad304b1c96ea
Irvin888 <irvin12138@gmail.com>
2024-10-15 16:54:32 -0700

    basic structure

---
+++ StressLess/StressLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  StressLessApp.swift
+//  StressLess
+//
+//  Created by Xucheng Zhao on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct StressLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StressLessTests/StressLessTests.swift
@@ -0,0 +1,17 @@
+//
+//  StressLessTests.swift
+//  StressLessTests
+//
+//  Created by Xucheng Zhao on 10/15/24.
+//
+
+import Testing
+@testable import StressLess
+
+struct StressLessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
